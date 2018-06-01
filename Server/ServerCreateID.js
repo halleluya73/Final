309316cf4ce5e78007ac83a6e3b3f11a6ca6024c
halleluya73 <mrhalleluya73@gmail.com>
2018-06-02 00:23:33 +0700

    8;p

--- Server/ServerCreateID.js
+++ Server/ServerCreateID.js
@@ -1,11 +1,11 @@
-
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+var util = require('util');
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'devteam',
-    password: 'password123',
+    user: 'devgame',
+    password: 'password',
     database: 'Game1'
 
 });
@@ -35,6 +35,7 @@ app.get('/user/add', function (req, res) {
 });
 app.get('/user/id', function (req, res) {
 
+
 	ShowUserID(function (err, result) {
 		res.end(result);
 	});
@@ -48,6 +49,7 @@ app.get('/user/login/:name', function (req, res) {
 		res.end(resualt);
 	});
 });
+
 
 
 var server = app.listen(8081, function () {
@@ -73,7 +75,8 @@ function AddUserID(ID, callback) {
 
     });
 }
-    function ShowUserID(callback) {
+function ShowUserID(callback) {
+	var json = "";
 		var sql = 'SELECT name,score FROM user ORDER BY score DESC limit 10';
 
         connection.query(sql, function (err,rows,fields) {
@@ -94,53 +97,54 @@ function AddUserID(ID, callback) {
     
             callback(null,json);
         });
-        app.get('/user/update', function (req, res) {
-            var name = req.query.name;
-            var score = req.query.score;
-            UpdateScore(name, score, function (er, result) {
-                res.end(result);
-            });
-        });
-        
-        
-        function UpdateScore(name, score, callback) {
-            var json = '';
-            var sql = util.format('UPDATE user SET score = %d WHERE name = "%s"', score, name);
-        
-            connection.query(sql,function (err) {
-        
-                var result = '[{"success":"true"}]';
-        
-                    if (err) {
-                        result = '[{"success":"false"}]';
-                        throw err;
-        
-                    }
-        
-                    callback(null, result);
-                });
-        }
-        
-        app.get('/user/loadscore/:name', function (req, res) {
-        
-            var login = req.params.name;
-        
-            LoadScore(login, function (err, result) {
-                res.end(result);
-            });
-        });
-        
-        
-        function LoadScore(name, callback) {
-            var json = '';
-            var sql = ('SELECT name,score FROM user WHERE name = ?');
-        
-            connection.query(sql, [name], function (err, rows, fields) {
-                if (err) throw err;
-        
-                json = JSON.stringify(rows);
-        
-                callback(null, json);
-            });
-    }
+	}
+
+app.get('/user/update', function (req, res) {
+	var name = req.query.name;
+	var score = req.query.score;
+	UpdateScore(name, score, function (er, result) {
+		res.end(result);
+	});
+});
+
+
+function UpdateScore(name, score, callback) {
+	var json = '';
+	var sql = util.format('UPDATE user SET score = %d WHERE name = "%s"', score, name);
+
+	connection.query(sql,function (err) {
+
+		var result = '[{"success":"true"}]';
+
+			if (err) {
+				result = '[{"success":"false"}]';
+				throw err;
+
+			}
+
+			callback(null, result);
+		});
+}
+
+app.get('/user/loadscore/:name', function (req, res) {
+
+	var login = req.params.name;
+
+	LoadScore(login, function (err, result) {
+		res.end(result);
+	});
+});
+
+
+function LoadScore(name, callback) {
+	var json = '';
+	var sql = ('SELECT name,score FROM user WHERE name = ?');
+
+	connection.query(sql, [name], function (err, rows, fields) {
+		if (err) throw err;
+
+		json = JSON.stringify(rows);
+
+		callback(null, json);
+	});
 }
